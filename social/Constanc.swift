ab42acd3c98d2ea68280923671ef94b3e25d0d39
okokoko <mario@Mario-Fancovics-MacBook-Pro-2.local>
2016-11-17 11:26:16 +0100

    vylepseny design

---
+++ social/Constanc.swift
@@ -0,0 +1,11 @@
+//
+//  Constanc.swift
+//  social
+//
+//  Created by Mario Fancovic on 17/11/2016.
+//  Copyright © 2016 Mario Fancovic. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 125.0 / 255.0 
---
+++ social/FancyTextField.swift
@@ -0,0 +1,30 @@
+//
+//  FancyTextField.swift
+//  social
+//
+//  Created by Mario Fancovic on 17/11/2016.
+//  Copyright © 2016 Mario Fancovic. All rights reserved.
+//
+
+import UIKit
+
+class FancyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 5.0
+        
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+        
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+}
