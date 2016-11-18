3e11e164d3df1a54320290e7f2600764fe81a6b3
okokoko <mario@Mario-Fancovics-MacBook-Pro-2.local>
2016-11-18 13:10:10 +0100

    email sign-in done

--- social/SignInVC.swift
+++ social/SignInVC.swift
@@ -11,11 +11,19 @@ import FBSDKCoreKit
 import FBSDKLoginKit
 import Firebase
 
-class SignInVC: UIViewController {
+class SignInVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet var emailField: FancyTextField!
+    @IBOutlet var pwdField: FancyTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // nasledujúce tri riadky sú pre opustenie klavesnice po texte
+        emailField.delegate = self
+        pwdField.delegate = self
+        self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: Selector("dismissKeyboard")))
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,13 +36,13 @@ class SignInVC: UIViewController {
         
         let facebookLogin = FBSDKLoginManager()
         
-        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+        facebookLogin.logIn(withReadPermissions: ["public_profile","email"], from: self) { (result, error) in
             if error != nil {
                 print("MARIO: Nebolo sa možné pripojiť k facebooku - \(error)")
             } else if result?.isCancelled == true {
                 print("MARIO: Používateľ zrušil autentifikáciu na Facebooku")
             } else {
-                print("MARIO: Autentifikásia úspešná")
+                print("MARIO: Autentifikásia u facebooku úspešná")
                 let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
                 self.firebaseAuth(credential)
             }
@@ -50,6 +58,36 @@ class SignInVC: UIViewController {
 
             }
         })
+    }
+    
+    @IBAction func signInTapped(_ sender: Any) {
+        if let email = emailField.text, let pasw = pwdField.text {
+            FIRAuth.auth()?.signIn(withEmail: email, password: pasw, completion: { (user, error) in
+                if error == nil {
+                    print("MARIO: Autentifikásia u firebase úspešná")
+                } else {
+                    FIRAuth.auth()?.createUser(withEmail: email, password: pasw, completion: { (user, error) in
+                        if error == nil {
+                            print("MARIO: Vytvorené nové konto pre usera \(email) u Firebase")
+                        } else {
+                            print("MARIO: Nebolo možné vytvoriť nové konto u firebase - \(error)")
+                        }
+                    })
+                }
+            })
+        }
+    }
+    
+    // Nasledujúce dve funkcie sú pre opustenie klávesnice, nezabudnúť delegate
+    func dismissKeyboard() {
+        pwdField.resignFirstResponder()
+        emailField.resignFirstResponder()
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        pwdField.resignFirstResponder()
+        emailField.resignFirstResponder()
+        return true
     }
 }
 
